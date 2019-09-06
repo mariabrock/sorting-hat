2e46d6e3dc826a5628e9730c349e58027051bd40
mariabrock <mariabrock414@gmail.com>
2019-09-05 20:01:52 -0500

    added jumbotron, form, and card structures

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
-console.log("You can pick your House AND your nose here.");
+console.log("You can pick your House AND your nose here!");
+
+const students = [
+    {
+        name: 'Maria',
+        house: 'Slytherin'
+    },
+    {
+        name: 'Phillip',
+        house: 'Ravenclaw'
+    },
+    {
+        name: 'Laura',
+        house: 'Hufflepuff'
+    },
+    {
+        name: 'Kelsey',
+        house: 'Gryffindoor'
+    },
+    {
+        name: 'Isis',
+        house: 'Ravenclaw'
+    }
+]
+const printToDOM = (toPrint, divId) => {
+    document.getElementById(divId).innerHTML = toPrint
+};
+
+const studentSorter = (studArray) => {
+    let domString = '<div class="row">'
+    for (let i = 0; i < studArray.length; i++) {
+        const currentStudent = studArray[i]
+        domString += `
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${currentStudent.name}</h5>
+                        <p class="card-text">${currentStudent.house}</p>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-danger">Expel</button>
+            </div>`
+    };
+    domString += '</div>'
+    printToDOM(domString, 'studentCard')
+  };
+
+  cardBuilder(students)
+
+// const buttonClick = (e) =>{
+//     const studentName = e.target.id;
+//     const selectedStudent = [];
+//     for (let i = 0; i < students.length; i++) {
+//         const student = students[i];
+//         if (students.house === house) {
+//             selectedStudent.push(student);
+//         };
+//     };
+//     cardBuilder(selectedStudent);
+// };
+
+// document.getElementById('zoe').addEventListener('click', buttonClickEvent)
