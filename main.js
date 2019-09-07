1665f4e3af884a5095ac8b662dd0865ef55eb52c
mariabrock <mariabrock414@gmail.com>
2019-09-07 12:03:13 -0500

    printed house and student name to page

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
 console.log("You can pick your House AND your nose here!");
 
-const students = [
-    {
-        name: 'Maria',
-        house: 'Slytherin'
-    },
-    {
-        name: 'Phillip',
-        house: 'Ravenclaw'
-    },
-    {
-        name: 'Laura',
-        house: 'Hufflepuff'
-    },
-    {
-        name: 'Kelsey',
-        house: 'Gryffindoor'
-    },
-    {
-        name: 'Isis',
-        house: 'Ravenclaw'
-    }
-];
-
 const schoolHouse = ['Gryffindoor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
 
 const printToDOM = (toPrint, divId) => {
-    document.getElementById(divId).innerHTML = toPrint
+    document.getElementById(divId).innerHTML += toPrint
 };
 
 const getForm = () => {
     let domString = `
-        <form id="nameForm">
+        <div id="nameForm">
         <div class="row">
             <div class="col">
-            <input type="text" class="form-control" placeholder="Full Name">
+            <input id="nameInput" type="text" class="form-control" placeholder="Full Name">
             </div>
-        <button type="submit" class="btn btn-dark">Sort Me!</button>
-    </form>`
+        <button id="sortme" type="submit" class="btn btn-dark">Sort Me!</button>
+    </div>`
     printToDOM(domString, 'getSortedForm')
   };
 
@@ -49,63 +26,30 @@ const buttonClick = (e) =>{
 
 document.getElementById('beginButton').addEventListener('click', buttonClick);
 
-const houseGenerator () => {
-    Math.floor(Math.random() * 5);
-    for (let i = 0; i < schoolHouse.length; i++) {
-        const wholeNumber = Math.floor();
-        const giveHouse = schoolHouse[i];
-        return (wholeNumber, giveHouse)
-    };
+const houseGenerator = (studentName) => {
+    const randomNumber = Math.floor(Math.random() * 4);
+        console.log(randomNumber)
+        const house = schoolHouse[randomNumber]
+        let domString = ` <div class="col-3">
+                         <div class="card">
+                             <div class="card-body">
+                                <h5 class="card-title">${studentName}</h5>
+                                <p class="card-text">${house}</p>
+                             </div>
+                         </div>
+                         <button type="button" class="btn btn-danger">Expel</button>
+                     </div>`
+    printToDOM(domString, 'studentCard');
 };
 
-
-const houseAssignment () => {
-    if (wholeNumber === 0){
-        return 'Gryffindoor';
-    };
-    if (wholeNumber === 1) {
-        return 'Slytherin';
-    };
-    if (wholeNumber === 2) { 
-        return 'Ravenclaw';
-    };
-    if (wholeNumber === 3) {
-        return 'Hufflepuff';
-    };
-};
+document.getElementById('getSortedForm').addEventListener('click', function (e) {
+    const buttonType = e.target.id;
+    if (buttonType === 'sortme') {
+        const studentName = document.getElementById('nameInput').value
+            console.log(studentName)
+            houseGenerator(studentName);
+    }
+});
 
 
-// const studentSorter = (studArray) => {
-//     let domString = '<div class="row">'
-//     for (let i = 0; i < studArray.length; i++) {
-//         const currentStudent = studArray[i]
-//         domString += `
-//             <div class="col-3">
-//                 <div class="card">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${currentStudent.name}</h5>
-//                         <p class="card-text">${currentStudent.house}</p>
-//                     </div>
-//                 </div>
-//                 <button type="button" class="btn btn-danger">Expel</button>
-//             </div>`
-//     };
-//     domString += '</div>'
-//     printToDOM(domString, 'studentCard')
-//   };
-
-//   cardBuilder(students)
-
-// const buttonClick = (e) =>{
-//     const studentName = e.target.id;
-//     const selectedStudent = [];
-//     for (let i = 0; i < students.length; i++) {
-//         const student = students[i];
-//         if (students.house === house) {
-//             selectedStudent.push(student);
-//         };
-//     };
-//     cardBuilder(selectedStudent);
-// };
-
-// document.getElementById('zoe').addEventListener('click', buttonClickEvent)
+document.getElementById('studentCard').addEventListener('click', buttonClick);
